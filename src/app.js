e5e59c11786a66059c62a574aa3b032d6b535cf2
Bereket Kibru <32790672+bereketkibru@users.noreply.github.com>
2020-02-24 13:04:33 +0300

    module 4

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-
+const { accounts, users, writeJSON } = require('./data.js')
 const express = require("express");
 
 const app = express();
@@ -11,12 +11,8 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
-const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8'
-);
-const accounts = JSON.parse(accountData);
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8'
-);
-const users = JSON.parse(userData);
+
+
 app.get('/', (req, res) => res.render('index', { title: "Account Summary", accounts }));
 app.get('/savings', (req, res) => res.render('account', { account: accounts.savings }));
 app.get('/checking', (req, res) => res.render('account', { account: accounts.checking }));
@@ -26,16 +22,14 @@ app.get('/transfer', (req, res) => res.render('transfer'));
 app.post('/transfer', (req, res) => {
     accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
     accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, '/json', 'accounts.json'), accountsJSON, 'utf8');
+    writeJSON();
     res.render('transfer', { message: "Transfer Completed" });
 });
 app.get('/payment', (req, res) => res.render('payment', { account: accounts.credit }));
 app.post('/payment', (req, res) => {
     accounts.credit.balance -= req.body.amount;
     accounts.credit.available += parseInt(req.body.amount, 10);
-    const accountsJSON = JSON.stringify(accounts, null, 4);
-    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8')
+    writeJSON();
     res.render('payment', { message: 'Payment Successful', account: accounts.credit })
 })
 app.listen(3000, () => console.log("PS project Running on port 3000!"));
